21d89d790c9a3c6588d0c09cf76e0521dd03f08e
Dick Tse <dicktse1@gmail.com>
2017-12-23 23:10:49 +0800

    Change ConsoleHelper from non-static class to static class.

--- Helper/ConsoleHelper.cs
+++ Helper/ConsoleHelper.cs
@@ -5,7 +5,7 @@ namespace Helper
     /// <summary>
     /// A class for providing some helper methods for console applications.
     /// </summary>
-    public class ConsoleHelper
+    public static class ConsoleHelper
     {
         public static void Pause()
         {
