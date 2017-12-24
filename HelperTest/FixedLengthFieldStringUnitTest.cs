d5ff626494ff50ab09141a91db178c5db2f3562a
dicktse <dicktse1@gmail.com>
2017-12-24 17:42:44 +0800

    Add unit tests for testing FixedLengthField and FixedLengthFieldString.

---
+++ HelperTest/FixedLengthFieldStringUnitTest.cs
@@ -0,0 +1,201 @@
+using Helper.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HelperTest
+{
+    [TestCategory("Unit Test")]
+    [TestClass]
+    public class FixedLengthFieldStringUnitTest
+    {
+        #region Unit tests for testing validations of the whole raw string
+        [TestMethod]
+        [ExpectedException(typeof(MalformedRawStringException))]
+        public void FixedLengthFieldString_RawStringTooShortShouldThrowMalformedRawStringException()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<string>("HKID", 10),
+                new FixedLengthField<string>("Name", 20)
+            };
+            string rawStr = "A123456CHAN TAI MAN";
+            try
+            {
+                new FixedLengthFieldString(rawStr, fields);
+            }
+            catch (MalformedRawStringException ex)
+            {
+                Assert.AreEqual("The raw string is too short.", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MalformedRawStringException))]
+        public void FixedLengthFieldString_RawStringTooLongShouldThrowMalformedRawStringException()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<string>("HKID", 10),
+                new FixedLengthField<string>("Name", 10),
+            };
+            string rawStr = "A123456(7)CHAN TAI MAN";
+            try
+            {
+                new FixedLengthFieldString(rawStr, fields);
+            }
+            catch (MalformedRawStringException ex)
+            {
+                Assert.AreEqual("The raw string is too long.", ex.Message);
+                throw;
+            }
+        }
+        #endregion
+
+        #region Unit tests for testing parsing of string into FixedLengthField.
+        [TestMethod]
+        public void FixedLengthFieldString_SingleStringIsParsedIntoFixedLengthFieldSuccessfully()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<string>("HKID", 10)
+            };
+            string rawStr = "A123456(7)";
+            FixedLengthFieldString fixedStr = new FixedLengthFieldString(rawStr, fields);
+            Assert.AreEqual("A123456(7)", fixedStr.Fields["HKID"]);
+        }
+        #endregion
+
+        #region Unit tests for testing parsing of integer into FixedLengthField.
+        [TestMethod]
+        public void FixedLengthFieldString_SingleIntegerIsParsedIntoFixedLengthFieldSuccessfully()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<int>("Age", 3)
+            };
+            string rawStr = "100";
+            FixedLengthFieldString fixedStr = new FixedLengthFieldString(rawStr, fields);
+            Assert.AreEqual(100, fixedStr.Fields["Age"]);
+        }
+
+        [TestMethod]
+        public void FixedLengthFieldString_IntegerWithTrailingSpaceShouldNotThrowException()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<int>("Age", 3)
+            };
+            string rawStr = "9  ";
+            try
+            {
+                new FixedLengthFieldString(rawStr, fields);
+            }
+            catch
+            {
+                Assert.Fail("Parsing integer with trailing space to FixedLengthFieldString should not throw any exception.");
+            }
+        }
+
+        [TestMethod]
+        public void FixedLengthFieldString_IntegerWithLeadingSpaceShouldNotThrowException()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<int>("Age", 3)
+            };
+            string rawStr = "  9";
+            try
+            {
+                new FixedLengthFieldString(rawStr, fields);
+            }
+            catch
+            {
+                Assert.Fail("Parsing integer with leading space to FixedLengthFieldString should not throw any exception.");
+            }
+        }
+
+        [TestMethod]
+        public void FixedLengthFieldString_IntegerWithTrailingSpaceIsParsedIntoFixedLengthFieldSuccessfully()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<int>("Age", 3)
+            };
+            string rawStr = "9  ";
+            FixedLengthFieldString fixedStr = new FixedLengthFieldString(rawStr, fields);
+            Assert.AreEqual(9, fixedStr.Fields["Age"]);
+        }
+
+        [TestMethod]
+        public void FixedLengthFieldString_IntegerWithLeadingSpaceIsParsedIntoFixedLengthFieldSuccessfully()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<int>("Age", 3)
+            };
+            string rawStr = "  9";
+            FixedLengthFieldString fixedStr = new FixedLengthFieldString(rawStr, fields);
+            Assert.AreEqual(9, fixedStr.Fields["Age"]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void FixedLengthFieldString_IntegerWithNonNumericalCharacterShouldThrowException()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection() {
+                new FixedLengthField<int>("Age", 3)
+            };
+            string rawStr = "99A";
+            FixedLengthFieldString fixedStr = new FixedLengthFieldString(rawStr, fields);
+        }
+        #endregion
+
+        #region Unit tests for testing parsing of DateTime value into FixedLengthField.
+        [TestMethod]
+        public void FixedLengthFieldString_DateTimeValueIsParsedIntoFixedLengthFieldSuccessfully()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection()
+            {
+                new FixedLengthField<DateTime>("DOB")
+            };
+            string rawStr = "19851013";
+            FixedLengthFieldString fixedStr = new FixedLengthFieldString(rawStr, fields);
+            Assert.AreEqual(new DateTime(1985, 10, 13), fixedStr.Fields["DOB"]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FixedLengthFieldString_ShouldThrowExceptionIfDateTimeLengthIsSpecified()
+        {
+            try
+            {
+                new FixedLengthField<DateTime>("DOB", 8);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual("Length should not be defined for any DateTime FieldLengthField object.", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void FixedLengthFieldString_InvalidDateTimeFormatStringShouldThrowException()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection()
+            {
+                new FixedLengthField<DateTime>("DOB") {DateTimeFormatString = "abcdef"}
+            };
+            string rawStr = "19851013";
+            new FixedLengthFieldString(rawStr, fields);
+        }
+        #endregion
+
+        [TestMethod]
+        public void FixedLengthFieldString_MultipleStringsAreAllParsedIntoFixedLengthFieldsSuccessfully()
+        {
+            FixedLengthFieldCollection fields = new FixedLengthFieldCollection()
+            {
+                new FixedLengthField<string>("HKID", 10),
+                new FixedLengthField<string>("Name", 20)
+            };
+            string rawStr = "A123456(7)CHAN TAI MAN        ";
+            FixedLengthFieldString fixedStr = new FixedLengthFieldString(rawStr, fields);
+            Assert.AreEqual("A123456(7)", fixedStr.Fields["HKID"]);
+            Assert.AreEqual("CHAN TAI MAN", fixedStr.Fields["Name"]);
+        }
+    }
+}
---
+++ HelperTest/FixedLengthFieldUnitTests.cs
@@ -0,0 +1,83 @@
+﻿using Helper.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelperTest
+{
+    [TestCategory("Unit Test")]
+    [TestClass]
+    public class FixedLengthFieldUnitTests
+    {
+        [TestMethod]
+        public void FixedLengthField_DecimalFieldShouldReturnDecimalValue()
+        {
+            int i = 9;
+            var field = new FixedLengthField<decimal>("Height", 3);
+            field.Value = i;
+            if (field.Value.GetType() != typeof(decimal))
+                Assert.Fail("Return value of decimal-type FixedLengthField is not decimal.");
+        }
+
+        [TestMethod]
+        public void FixedLengthField_DecimalValueInStringShouldBeParsedIntoFixedLengthFieldSuccessfully()
+        {
+            var field = new FixedLengthField<decimal>("Height", 3);
+            field.PaddedString = "9.1";
+            Assert.AreEqual(9.1M, field.Value);
+        }
+
+        [TestMethod]
+        public void FixedLengthField_IntegerFieldShouldReturnIntegerValue()
+        {
+            decimal d = 9.0M;
+            var field = new FixedLengthField<int>("Height", 3);
+            field.Value = (int)d;
+            if (field.Value.GetType() != typeof(int))
+                Assert.Fail("Return value of integer-type FixedLengthField is not integer.");
+        }
+
+        [TestMethod]
+        public void FixedLengthField_IntegerFieldWithNegativeSignInPaddedStringShouldNotThrowException()
+        {
+            try
+            {
+                var field = new FixedLengthField<int>("Age", 3);
+                field.PaddedString = "-9 ";
+            }
+            catch (OverflowException)
+            {
+                Assert.Fail("Assigning negative number into PaddedString property of integer-type FixedLengthField should not throw Overflow exception.");
+            }
+        }
+
+        [TestMethod]
+        public void FixedLengthField_IntegerFieldPaddedStringWithDefaultPaddingPaddingCharPositionShouldPadTrailingSpace()
+        {
+            var field = new FixedLengthField<int>("Age", 3);
+            field.Value = 9;
+            Assert.AreEqual("9  ", field.PaddedString);
+        }
+
+        public void FixedLengthField_IntegerFieldPaddedStringWithPaddingPaddingCharPositionLeftShouldPadLeadingingSpace()
+        {
+            var field = new FixedLengthField<int>("Age", 3)
+            {
+                PaddingCharPosition = PaddingCharPosition.Left
+            };
+            field.Value = 9;
+            Assert.AreEqual("  9", field.PaddedString);
+        }
+
+        public void FixedLengthField_IntegerFieldPaddedStringWithPaddingPaddingCharPositionRightShouldPadTrailingSpace()
+        {
+            var field = new FixedLengthField<int>("Age", 3)
+            {
+                PaddingCharPosition = PaddingCharPosition.Right
+            };
+            field.Value = 9;
+            Assert.AreEqual("9  ", field.PaddedString);
+        }
+    }
+}
