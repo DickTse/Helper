5f2175727c697c266bc2fc20fdc80a75501ae86c
dicktse <dicktse1@gmail.com>
2018-01-13 17:45:15 +0800

    Add TODO task into LogPolicy.

--- Helper/AOP/LogPolicy.cs
+++ Helper/AOP/LogPolicy.cs
@@ -1,11 +1,10 @@
-﻿using System;
-
-namespace Helper.AOP
+﻿namespace Helper.AOP
 {
     public static class LogPolicy
     {
         private static ILogger _logger;
 
+        // TODO: Registering logger in this way may lead to race conditions. Have to find a better way to register logger.
         public static void Register(ILogger logger)
         {
             _logger = logger;
